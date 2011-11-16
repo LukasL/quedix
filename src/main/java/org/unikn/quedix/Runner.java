085ce6a308c795e5a683296a40b2b4be1105d534
LukasL <lukas.lewandowski@uni-konstanz.de>
2011-11-16 15:32:14 +0100

    updated distributer for distribution for one single XML document and collections with less than 10 documents (bugfix)

--- src/main/java/org/unikn/quedix/Runner.java
+++ src/main/java/org/unikn/quedix/Runner.java
@@ -21,151 +21,157 @@ import org.unikn.quedix.socket.SocketClient;
  */
 public class Runner {
 
-    /** Example document for importing and querying. */
-    public static final String DOC = "factbook";
-    /** REST path. */
-    public static final String REST_COL = "rest/";
-    /** HTTP path. */
-    public static final String HTTP = "http://";
-    /** Host name. */
-    public static final String HOST = "aalto.disy.inf.uni-konstanz.de";
-    /** User name. */
-    public static final String USER = "admin";
-    /** User password. */
-    public static final String PW = "admin";
+	/** Example document for importing and querying. */
+	public static final String DOC = "factbook";
+	/** REST path. */
+	public static final String REST_COL = "rest/";
+	/** HTTP path. */
+	public static final String HTTP = "http://";
+	/** Host name. */
+	public static final String HOST = "aalto.disy.inf.uni-konstanz.de";
+	/** User name. */
+	public static final String USER = "admin";
+	/** User password. */
+	public static final String PW = "admin";
 
-    /**
-     * Main.
-     * 
-     * @param args
-     *            Program arguments are input paths to map and reduce XQuery
-     *            files or XML collection.
-     * @throws IOException
-     *             File not found.
-     * @throws QueryException
-     *             Query exception.
-     */
-    public static void main(final String[] args) throws IOException, QueryException {
-        long start = System.nanoTime();
+	/**
+	 * Main.
+	 * 
+	 * @param args
+	 *            Program arguments are input paths to map and reduce XQuery
+	 *            files or XML collection.
+	 * @throws IOException
+	 *             File not found.
+	 * @throws QueryException
+	 *             Query exception.
+	 */
+	public static void main(final String[] args) throws IOException,
+			QueryException {
+		long start = System.nanoTime();
 
-        if (args.length == 1)
-            // Map
-            new Runner(args[0], ClientType.SOCKETS);
-        else if (args.length == 2)
-            // Distribution of collection
-            new Runner(args[0], args[1]);
-        else if (args.length == 3)
-            // Map & Reduce
-            new Runner(args[0], args[1], ClientType.REST);
-        else
-            // Error message because false user input parameters
-            System.err.println("False input parameters.");
-        long end = System.nanoTime() - start;
-        System.out.println("\nComplete execution time: " + end / 1000000 + " ms \n");
+		if (args.length == 1)
+			// Map
+			new Runner(args[0], ClientType.SOCKETS);
+		else if (args.length == 2)
+			// Distribution of collection
+			new Runner(args[0], args[1]);
+		else if (args.length == 3)
+			// Map & Reduce
+			new Runner(args[0], args[1], ClientType.REST);
+		else
+			// Error message because false user input parameters
+			System.err.println("False input parameters.");
+		long end = System.nanoTime() - start;
+		System.out.println("\nComplete execution time: " + end / 1000000
+				+ " ms \n");
 
-    }
+	}
 
-    /**
-     * Constructor distributes an XML collection.
-     * 
-     * @throws IOException
-     *             XML directory not found.
-     */
-    public Runner(final String xmlDir, final String name) throws IOException {
-        // Client cl = new RestClient(initHttpDataServers());
-        Client cl = new SocketClient(initBaseXClients());
-        try {
-            cl.distributeCollection(xmlDir, name);
-        } catch (final Exception exc) {
-            exc.printStackTrace();
-        }
-    }
+	/**
+	 * Constructor distributes an XML collection.
+	 * 
+	 * @throws IOException
+	 *             XML directory not found.
+	 */
+	public Runner(final String xmlDir, final String name) throws IOException {
+		Client cl = new RestClient(initHttpDataServers());
+		// Client cl = new SocketClient(initBaseXClients());
+		try {
+			cl.distributeCollection(xmlDir, name);
+		} catch (final Exception exc) {
+			exc.printStackTrace();
+		}
+	}
 
-    /**
-     * Constructor creates and executes a map job.
-     * 
-     * @param xq
-     *            XQuery file.
-     * @param type
-     *            Client type, either {@link ClientType#REST} or {@link ClientType#SOCKETS}.
-     * @throws IOException
-     *             XQ file not found.
-     */
-    public Runner(final String xq, final ClientType type) throws IOException {
-        if (type == ClientType.REST)
-            map(new MapClient(new RestClient(initHttpDataServers()), new File(xq)));
-        else {
-            SocketClient client = new SocketClient(initBaseXClients());
-            map(new MapClient(client, new File(xq)));
-            client.shutdownClients();
-        }
-    }
+	/**
+	 * Constructor creates and executes a map job.
+	 * 
+	 * @param xq
+	 *            XQuery file.
+	 * @param type
+	 *            Client type, either {@link ClientType#REST} or
+	 *            {@link ClientType#SOCKETS}.
+	 * @throws IOException
+	 *             XQ file not found.
+	 */
+	public Runner(final String xq, final ClientType type) throws IOException {
+		if (type == ClientType.REST)
+			map(new MapClient(new RestClient(initHttpDataServers()), new File(
+					xq)));
+		else {
+			SocketClient client = new SocketClient(initBaseXClients());
+			map(new MapClient(client, new File(xq)));
+			client.shutdownClients();
+		}
+	}
 
-    /**
-     * Constructor creates and executes a map and a reduce job.
-     * 
-     * @param mapXq
-     *            XQuery map file.
-     * @param reduceXq
-     *            XQuery reduce file.
-     * @param type
-     *            Client type, either {@link ClientType#REST} or {@link ClientType#SOCKETS}.
-     * @throws IOException
-     *             XQ file not found.
-     * @throws QueryException
-     *             Query exception.
-     */
-    public Runner(final String mapXq, final String reduceXq, final ClientType type) throws IOException,
-        QueryException {
-        if (type == ClientType.REST)
-            map(new MapClient(new RestClient(initHttpDataServers()), new File(mapXq), new ReduceClient(
-                new File(reduceXq))));
-        else {
-            SocketClient client = new SocketClient(initBaseXClients());
-            map(new MapClient(client, new File(mapXq), new ReduceClient(new File(reduceXq))));
-            client.shutdownClients();
-        }
+	/**
+	 * Constructor creates and executes a map and a reduce job.
+	 * 
+	 * @param mapXq
+	 *            XQuery map file.
+	 * @param reduceXq
+	 *            XQuery reduce file.
+	 * @param type
+	 *            Client type, either {@link ClientType#REST} or
+	 *            {@link ClientType#SOCKETS}.
+	 * @throws IOException
+	 *             XQ file not found.
+	 * @throws QueryException
+	 *             Query exception.
+	 */
+	public Runner(final String mapXq, final String reduceXq,
+			final ClientType type) throws IOException, QueryException {
+		if (type == ClientType.REST)
+			map(new MapClient(new RestClient(initHttpDataServers()), new File(
+					mapXq), new ReduceClient(new File(reduceXq))));
+		else {
+			SocketClient client = new SocketClient(initBaseXClients());
+			map(new MapClient(client, new File(mapXq), new ReduceClient(
+					new File(reduceXq))));
+			client.shutdownClients();
+		}
 
-    }
+	}
 
-    /**
-     * Executes mapping functions.
-     * 
-     * @param mapper
-     *            {@link MapClient} instance.
-     */
-    private void map(final MapClient mapper) {
-        mapper.distribute();
-        mapper.execute();
-        mapper.cleanup();
-    }
+	/**
+	 * Executes mapping functions.
+	 * 
+	 * @param mapper
+	 *            {@link MapClient} instance.
+	 */
+	private void map(final MapClient mapper) {
+		mapper.distribute();
+		mapper.execute();
+		mapper.cleanup();
+	}
 
-    /**
-     * Initializes the example servers for REST calls.
-     * 
-     * @return {@link Map} of server mappings.
-     */
-    private Map<String, String> initHttpDataServers() {
-        Map<String, String> dataServers = new HashMap<String, String>();
-        dataServers.put(HTTP + HOST + ":8984/", REST_COL);
-        dataServers.put(HTTP + HOST + ":8986/", REST_COL);
-        dataServers.put(HTTP + HOST + ":8988/", REST_COL);
-        return dataServers;
-    }
+	/**
+	 * Initializes the example servers for REST calls.
+	 * 
+	 * @return {@link Map} of server mappings.
+	 */
+	private Map<String, String> initHttpDataServers() {
+		Map<String, String> dataServers = new HashMap<String, String>();
+		dataServers.put(HTTP + HOST + ":8984/", REST_COL);
+		dataServers.put(HTTP + HOST + ":8986/", REST_COL);
+		dataServers.put(HTTP + HOST + ":8988/", REST_COL);
+		return dataServers;
+	}
 
-    /**
-     * Initialization of BaseX clients.
-     * 
-     * @return {@link Map} of connected BaseX clients.
-     * @throws IOException
-     *             Exception occurred, e.g. servers are not running.
-     */
-    private Map<String, BaseXClient> initBaseXClients() throws IOException {
-        Map<String, BaseXClient> clients = new HashMap<String, BaseXClient>();
-        clients.put("site1", new BaseXClient(HOST, 1980, USER, PW));
-        clients.put("site2", new BaseXClient(HOST, 1981, USER, PW));
-        clients.put("site3", new BaseXClient(HOST, 1982, USER, PW));
-        return clients;
-    }
+	/**
+	 * Initialization of BaseX clients.
+	 * 
+	 * @return {@link Map} of connected BaseX clients.
+	 * @throws IOException
+	 *             Exception occurred, e.g. servers are not running.
+	 */
+	private Map<String, BaseXClient> initBaseXClients() throws IOException {
+		Map<String, BaseXClient> clients = new HashMap<String, BaseXClient>();
+		clients.put("site1", new BaseXClient(HOST, 1980, USER, PW));
+		clients.put("site2", new BaseXClient(HOST, 1981, USER, PW));
+		clients.put("site3", new BaseXClient(HOST, 1982, USER, PW));
+		return clients;
+	}
 
 }
--- src/main/java/org/unikn/quedix/core/Client.java
+++ src/main/java/org/unikn/quedix/core/Client.java
@@ -11,68 +11,72 @@ import java.util.List;
  */
 public interface Client {
 
-    /**
-     * This method distributes a map.xq or a reduce.xq to the data server.
-     * 
-     * @param xq
-     *            An XQ file as byte array.
-     * @return <code>true</code> if the distribution has been successful, <code>false</code> otherwise.
-     */
-    public boolean distributeXq(final byte[] xq);
+	/**
+	 * This method distributes a map.xq or a reduce.xq to the data server.
+	 * 
+	 * @param xq
+	 *            An XQ file as byte array.
+	 * @return <code>true</code> if the distribution has been successful,
+	 *         <code>false</code> otherwise.
+	 */
+	public boolean distributeXq(final byte[] xq);
 
-    /**
-     * Executes XQ files on the server and receives results.
-     * 
-     * @param xq
-     *            XQ file - map oder reduce file.
-     * @return Complete results of XQ files.
-     */
-    public String[] execute(final String xq);
+	/**
+	 * Executes XQ files on the server and receives results.
+	 * 
+	 * @param xq
+	 *            XQ file - map oder reduce file.
+	 * @return Complete results of XQ files.
+	 */
+	public String[] execute(final String xq);
 
-    /**
-     * Executes XQ files on the server and receives results.
-     * 
-     * @param xq
-     *            XQ file - map oder reduce file.
-     * @param output
-     *            Stream for writing results.
-     * @return Complete results of XQ files.
-     */
-    public void execute(final String xq, final OutputStream output);
+	/**
+	 * Executes XQ files on the server and receives results.
+	 * 
+	 * @param xq
+	 *            XQ file - map oder reduce file.
+	 * @param output
+	 *            Stream for writing results.
+	 * @return Complete results of XQ files.
+	 */
+	public void execute(final String xq, final OutputStream output);
 
-    /**
-     * This method delete map.xq or reduce.xq files from the data server.
-     * 
-     * @return <code>true</code> if the deletion has been successful, <code>false</code> otherwise.
-     */
-    public boolean delete();
+	/**
+	 * This method delete map.xq or reduce.xq files from the data server.
+	 * 
+	 * @return <code>true</code> if the deletion has been successful,
+	 *         <code>false</code> otherwise.
+	 */
+	public boolean delete();
 
-    /**
-     * Checks if MapperDb exists already.
-     * 
-     * @return List of servers where MapperDb do not exist.
-     */
-    public List<String> checkMapperDb();
+	/**
+	 * Checks if MapperDb exists already.
+	 * 
+	 * @return List of servers where MapperDb do not exist.
+	 */
+	public List<String> checkMapperDb();
 
-    /**
-     * Creates MapperDb on server.
-     * 
-     * @param dataServer
-     *            Server.
-     */
-    public void createMapperDb(final String dataServer);
+	/**
+	 * Creates MapperDb on server.
+	 * 
+	 * @param dataServer
+	 *            Server.
+	 */
+	public void createMapperDb(final String dataServer);
 
-    /**
-     * Distributes an XML collection to the available servers.
-     * 
-     * @param collection
-     *            Path of XML directory.
-     * @param name
-     *            The name of XML collection.
-     * @return <code>true</code> if the distribution was successful, <code>false</code> otherwise.
-     * @exception Exception
-     *                Exception occurred.
-     */
-    public boolean distributeCollection(final String collection, final String name) throws Exception;
+	/**
+	 * Distributes an XML collection to the available servers.
+	 * 
+	 * @param collection
+	 *            Path of XML directory.
+	 * @param name
+	 *            The name of XML collection.
+	 * @return <code>true</code> if the distribution was successful,
+	 *         <code>false</code> otherwise.
+	 * @exception Exception
+	 *                Exception occurred.
+	 */
+	public boolean distributeCollection(final String collection,
+			final String name) throws Exception;
 
 }
