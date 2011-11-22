79b3dcd4369e2deeedbec184b869e6ed747db143
LukasL <lukas.lewandowski@uni-konstanz.de>
2011-11-22 16:40:12 +0100

    run xq refactoring

--- src/main/java/org/unikn/quedix/rest/DistributionService.java
+++ src/main/java/org/unikn/quedix/rest/DistributionService.java
@@ -1,5 +1,10 @@
 package org.unikn.quedix.rest;
 
+import static org.unikn.quedix.rest.Constants.CONTENT_TYPE_STRING;
+import static org.unikn.quedix.rest.Constants.POST;
+import static org.unikn.quedix.rest.Constants.PUT;
+import static org.unikn.quedix.rest.Constants.TEXT_XML;
+
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,11 +14,6 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import static org.unikn.quedix.rest.Constants.CONTENT_TYPE_STRING;
-import static org.unikn.quedix.rest.Constants.POST;
-import static org.unikn.quedix.rest.Constants.PUT;
-import static org.unikn.quedix.rest.Constants.TEXT_XML;
-
 /**
  * This class is responsible to distribute a collection of XML documents to the
  * data servers via HTTP.
@@ -22,196 +22,251 @@ import static org.unikn.quedix.rest.Constants.TEXT_XML;
  */
 public class DistributionService {
 
-    /** Registered servers to connect to. */
-    private String[] mServers;
-    /** {@link OutputStream} from REST request. */
-    private OutputStream output;
-    /** {@link HttpURLConnection} connection instance. */
-    private HttpURLConnection conn;
+	/** XQ for refactoring subcollection workaround. */
+	private static final String RQ = "refactor_subcollection.xq";
+	/** Registered servers to connect to. */
+	private String[] mServers;
+	/** {@link OutputStream} from REST request. */
+	private OutputStream output;
+	/** {@link HttpURLConnection} connection instance. */
+	private HttpURLConnection conn;
 
-    /**
-     * Constructor creates REST calls to one server.
-     * 
-     * @param server
-     *            server address.
-     */
-    public DistributionService(final String server) {
-        this(new String[] {
-            server
-        });
-    }
+	/**
+	 * Constructor creates REST calls to one server.
+	 * 
+	 * @param server
+	 *            server address.
+	 */
+	public DistributionService(final String server) {
+		this(new String[] { server });
+	}
 
-    /**
-     * Constructor creates a service for REST calls to several servers, e.g.,
-     * for replication reasons.
-     * 
-     * @param servers
-     *            Registered servers.
-     */
-    public DistributionService(final String[] servers) {
-        mServers = servers;
-    }
+	/**
+	 * Constructor creates a service for REST calls to several servers, e.g.,
+	 * for replication reasons.
+	 * 
+	 * @param servers
+	 *            Registered servers.
+	 */
+	public DistributionService(final String[] servers) {
+		mServers = servers;
+	}
 
-    /**
-     * This method creates a new document, or if one is existing with this name,
-     * the old one will be replaced.
-     * 
-     * @param name
-     *            Name of document.
-     * @param document
-     *            Document content.
-     * @return <code>true</code> if call was successful, <code>false</code> otherwise.
-     * @throws Exception
-     *             occurred with remote call.
-     */
-    public boolean update(final String name, final InputStream document) throws IOException {
-        for (final String host : mServers) {
+	/**
+	 * This method creates a new document, or if one is existing with this name,
+	 * the old one will be replaced.
+	 * 
+	 * @param name
+	 *            Name of document.
+	 * @param document
+	 *            Document content.
+	 * @return <code>true</code> if call was successful, <code>false</code>
+	 *         otherwise.
+	 * @throws Exception
+	 *             occurred with remote call.
+	 */
+	public boolean update(final String name, final InputStream document)
+			throws IOException {
+		for (final String host : mServers) {
 
-            URL url = new URL(host + "/" + name);
-            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-            conn.setDoOutput(true);
-            conn.setRequestProperty(CONTENT_TYPE_STRING, TEXT_XML);
-            conn.setRequestMethod(PUT);
-            OutputStream out = new BufferedOutputStream(conn.getOutputStream());
-            int i;
-            while((i = document.read()) != -1)
-                out.write(i);
-            out.close();
-            int code = conn.getResponseCode();
-            conn.disconnect();
-            return code == 201;
+			URL url = new URL(host + "/" + name);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestProperty(CONTENT_TYPE_STRING, TEXT_XML);
+			conn.setRequestMethod(PUT);
+			OutputStream out = new BufferedOutputStream(conn.getOutputStream());
+			int i;
+			while ((i = document.read()) != -1)
+				out.write(i);
+			out.close();
+			int code = conn.getResponseCode();
+			conn.disconnect();
+			return code == 201;
 
-        }
-        return false;
-    }
+		}
+		return false;
+	}
 
-    /**
-     * This method adds a document to an existing collection.
-     * 
-     * @param name
-     *            Name of document.
-     * @param document
-     *            Document content.
-     * @return <code>true</code> if call was successful, <code>false</code> otherwise.
-     * @throws Exception
-     *             occurred with remote call.
-     */
-    public boolean add(final String name, final InputStream document) throws IOException {
-        for (final String host : mServers) {
+	/**
+	 * This method adds a document to an existing collection.
+	 * 
+	 * @param name
+	 *            Name of document.
+	 * @param document
+	 *            Document content.
+	 * @return <code>true</code> if call was successful, <code>false</code>
+	 *         otherwise.
+	 * @throws Exception
+	 *             occurred with remote call.
+	 */
+	public boolean add(final String name, final InputStream document)
+			throws IOException {
+		for (final String host : mServers) {
 
-            URL url = new URL(host + "/" + name);
-            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-            conn.setDoOutput(true);
-            conn.setRequestProperty(CONTENT_TYPE_STRING, TEXT_XML);
-            conn.setRequestMethod(POST);
-            OutputStream out = new BufferedOutputStream(conn.getOutputStream());
-            int i;
-            while((i = document.read()) != -1)
-                out.write(i);
-            out.close();
-            int code = conn.getResponseCode();
-            if (code == 200)
-                while(conn.getInputStream().read() != -1)
-                    ;
-            conn.disconnect();
-            return code == 200;
+			URL url = new URL(host + "/" + name);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestProperty(CONTENT_TYPE_STRING, TEXT_XML);
+			conn.setRequestMethod(POST);
+			OutputStream out = new BufferedOutputStream(conn.getOutputStream());
+			int i;
+			while ((i = document.read()) != -1)
+				out.write(i);
+			out.close();
+			int code = conn.getResponseCode();
+			if (code == 200)
+				while (conn.getInputStream().read() != -1)
+					;
+			conn.disconnect();
+			return code == 200;
 
-        }
-        return false;
-    }
+		}
+		return false;
+	}
 
-    /**
-     * This method is responsible to prepare a PUT call to the server.
-     * 
-     * @param name
-     *            The collection name.
-     * @throws IOException
-     *             Exception occurred.
-     */
-    public void initUpdate(final String name) throws IOException {
-        URL url = new URL(mServers[0] + "/" + name);
-        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-        conn.setDoOutput(true);
-        conn.setRequestMethod(PUT);
-        conn.setRequestProperty(CONTENT_TYPE_STRING, TEXT_XML);
-        output = new BufferedOutputStream(conn.getOutputStream());
-        this.conn = conn;
-    }
+	/**
+	 * This method is responsible to prepare a PUT call to the server.
+	 * 
+	 * @param name
+	 *            The collection name.
+	 * @throws IOException
+	 *             Exception occurred.
+	 */
+	public void initUpdate(final String name) throws IOException {
+		URL url = new URL(mServers[0] + "/" + name);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod(PUT);
+		conn.setRequestProperty(CONTENT_TYPE_STRING, TEXT_XML);
+		output = new BufferedOutputStream(conn.getOutputStream());
+		this.conn = conn;
+	}
 
-    /**
-     * This method is responsible to execute the prepared PUT call to the
-     * server.
-     * 
-     * @return <code>true</code> if the HTTP request has been successful, <code>false</code> otherwise.
-     * @throws IOException
-     *             Exception occurred.
-     */
-    public boolean execUpdate() throws IOException {
-        output.close();
-        conn.connect();
-        int code = conn.getResponseCode();
-        System.out.println("code: " + code);
-        if (code == 201) {
-            BufferedReader r = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String l;
-            while((l = r.readLine()) != null)
-                System.out.println(l);
-            r.close();
-        } else {
-            String l;
-            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-            while((l = reader.readLine()) != null) {
-                System.out.println(l);
-            }
-        }
-        conn.disconnect();
-        return code == 201;
-    }
+	/**
+	 * This method is responsible to execute the prepared PUT call to the
+	 * server.
+	 * 
+	 * @return <code>true</code> if the HTTP request has been successful,
+	 *         <code>false</code> otherwise.
+	 * @throws IOException
+	 *             Exception occurred.
+	 */
+	public boolean execUpdate() throws IOException {
+		output.close();
+		conn.connect();
+		int code = conn.getResponseCode();
+		System.out.println("code: " + code);
+		if (code == 201) {
+			BufferedReader r = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+			String l;
+			while ((l = r.readLine()) != null)
+				System.out.println(l);
+			r.close();
+		} else {
+			String l;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					conn.getErrorStream()));
+			while ((l = reader.readLine()) != null) {
+				System.out.println(l);
+			}
+		}
+		conn.disconnect();
+		return code == 201;
+	}
 
-    /**
-     * This method is responsible to prepare a POST request to add documents to
-     * an existing collection.
-     * 
-     * @param name
-     *            The name of the collection.
-     * @throws IOException
-     *             Exception occurred.
-     */
-    public void initAdd(final String name) throws IOException {
-        URL url = new URL(mServers[0] + "/" + name);
-        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-        conn.setDoOutput(true);
-        conn.setRequestProperty(CONTENT_TYPE_STRING, TEXT_XML);
-        conn.setRequestMethod(POST);
-        OutputStream out = new BufferedOutputStream(conn.getOutputStream());
-        output = out;
-        this.conn = conn;
-    }
+	/**
+	 * This method is responsible to prepare a POST request to add documents to
+	 * an existing collection.
+	 * 
+	 * @param name
+	 *            The name of the collection.
+	 * @throws IOException
+	 *             Exception occurred.
+	 */
+	public void initAdd(final String name) throws IOException {
+		URL url = new URL(mServers[0] + "/" + name);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestProperty(CONTENT_TYPE_STRING, TEXT_XML);
+		conn.setRequestMethod(POST);
+		OutputStream out = new BufferedOutputStream(conn.getOutputStream());
+		output = out;
+		this.conn = conn;
+	}
 
-    /**
-     * This method is responsible to execute the prepared HTTP POST request.
-     * 
-     * @return <code>true</code> if the call has been successful, <code>false</code> otherwise.
-     * @throws IOException
-     *             Exception occurred.
-     */
-    public boolean execAdd() throws IOException {
-        output.close();
-        int code = conn.getResponseCode();
-        if (code == 200)
-            while(conn.getInputStream().read() != -1)
-                ;
-        conn.disconnect();
-        return code == 200;
-    }
+	/**
+	 * This method is responsible to execute the prepared HTTP POST request.
+	 * 
+	 * @return <code>true</code> if the call has been successful,
+	 *         <code>false</code> otherwise.
+	 * @throws IOException
+	 *             Exception occurred.
+	 */
+	public boolean execAdd() throws IOException {
+		output.close();
+		int code = conn.getResponseCode();
+		if (code == 200)
+			while (conn.getInputStream().read() != -1)
+				;
+		conn.disconnect();
+		return code == 200;
+	}
 
-    /**
-     * {@link OutputStream} from REST request.
-     * 
-     * @return {@link OutputStream} from REST request.
-     */
-    public OutputStream getOutputStream() {
-        return output;
-    }
+	/**
+	 * {@link OutputStream} from REST request.
+	 * 
+	 * @return {@link OutputStream} from REST request.
+	 */
+	public OutputStream getOutputStream() {
+		return output;
+	}
+
+	/**
+	 * Creates an empty collection for refactoring of subcollection workaround.
+	 * 
+	 * @param name
+	 *            Name of the collection.
+	 * @return <code>true</code> if the creation was successful,
+	 *         <code>false</code> otherwise.
+	 * @throws IOException
+	 *             Exception occurred.
+	 */
+	public boolean createEmptyCollection(final String name) throws IOException {
+		URL url = new URL(mServers[0] + "/" + name);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod(PUT);
+		int code = conn.getResponseCode();
+		if (code == 201)
+			while (conn.getInputStream().read() != -1)
+				;
+		conn.disconnect();
+		return code == 201;
+	}
+
+	/**
+	 * Executes refactoring of subcollection workaround.
+	 * 
+	 * @param collection
+	 *            Name of the collection.
+	 * @param sub
+	 *            Name of subcollection.
+	 * @return <code>true</code> if the creation was successful,
+	 *         <code>false</code> otherwise.
+	 * @throws IOException
+	 *             Exception occurred.
+	 */
+	public boolean runRefactoring(final String sub, final String collection)
+			throws IOException {
+		URL url = new URL(mServers[0] + "?run=" + RQ + "&subcollection=" + sub + "&collectoinname=" + sub);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		int code = conn.getResponseCode();
+		if (code == 200)
+			while (conn.getInputStream().read() != -1)
+				;
+		conn.disconnect();
+		return code == 200;
+	}
 
 }
